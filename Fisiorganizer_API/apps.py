2b1f2bafeb41490d92a9fa8d892e7e8df401df34
rogerdenisvieira <rogerdenis@gmail.com>
2017-06-30 20:25:56 -0300

    Commit inicial

---
+++ Fisiorganizer_API/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FisiorganizerApiConfig(AppConfig):
+    name = 'Fisiorganizer_API'
---
+++ Fisiorganizer_SITE/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+
+# alunos, professores, exercicios, aulas (exercicio + aluno + professor), agenda (aulas+data)
