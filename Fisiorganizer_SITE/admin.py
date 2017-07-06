9ea940266a44be65ea56d9a6bac4092c16a7591c
Roger D. Vieira <rogerdenis@gmail.com>
2017-07-06 16:04:09 -0300

    Update admin.py
    removed Trainning from admin.py

--- Fisiorganizer_SITE/admin.py
+++ Fisiorganizer_SITE/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from Fisiorganizer_SITE.models import User_extra, Session, Exercise, Customer, Trainning
+from Fisiorganizer_SITE.models import User_extra, Session, Exercise, Customer
 
 # Register your models here.
 admin.site.register(User_extra)
 admin.site.register(Session)
 admin.site.register(Exercise)
 admin.site.register(Customer)
-admin.site.register(Trainning)
-
