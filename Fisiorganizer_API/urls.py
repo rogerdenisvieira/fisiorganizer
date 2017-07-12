76053d17ed76d7a0c3a8bcd11535698430b6d4da
rogervieira <rogerdenis@gmail.com>
2017-07-12 18:45:37 -0300

    created API Rest

--- Fisiorganizer_API/urls.py
+++ Fisiorganizer_API/urls.py
@@ -1,10 +1,10 @@
 from django.conf import settings
 from django.conf.urls import url
 from django.views.static import serve
-from Fisiorganizer_API.views import CustomerList
+from Fisiorganizer_API import views
 
 
 urlpatterns = [
-    url(r'^customer/', CustomerList.as_view(), name='customer_list'),
-    #url(r'^customer/(?P<id>[0-9]{1})', CustomerList.as_view(), name='customer_list'),
+    url(r'^customer/(?P<id>[0-9]{1})', views.get_customer_by_id, name='customer_list'),
+    url(r'^customer/$', views.get_all_customers, name='customer_list_all'),
 ]
--- Fisiorganizer_API/views.py
+++ Fisiorganizer_API/views.py
@@ -1,14 +1,27 @@
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
 from rest_framework.response import Response
 from Fisiorganizer_SITE.models import Customer
 from Fisiorganizer_API.serializers import CustomerSerializer
 
 
-class CustomerList(APIView):
-    def get(self, request, id):
-        customers = Customer.objects.all()
-        serializer = CustomerSerializer(customers, many=True)
-        return Response(serializer.data)
+@api_view(['GET'])
+def get_customer_by_id(request, id):
+    customers = Customer.objects.filter(id=id)
+    serializer = CustomerSerializer(customers, many=True)
+    print(id)
+    return Response(serializer.data)
 
-    def post(self, request):
-        pass
+
+@api_view(['GET'])
+def get_all_customers(request):
+    customers = Customer.objects.all()
+    serializer = CustomerSerializer(customers, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_customer(request):
+    print('passei pelo POST')
+    pass
